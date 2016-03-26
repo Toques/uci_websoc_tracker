71ae0a383eb09edac44e5525b165bacd1703eb7d
Calvin Wong <toques528@gmail.com>
2016-03-25 19:45:57 -0700

    SMS alert functionality
    Experimentation with Twilio's SMS service with a trial account. Program
    currently successfully HTTP POST requests a course, parses it, and sends
    a conditional SMS

--- uci_websoc_tracker_experiment.py
+++ uci_websoc_tracker_experiment.py
@@ -1,23 +1,37 @@
 import requests
+import smtplib
+from twilio.rest import TwilioRestClient
 
 def main():
-    r = requests.post('https://www.reg.uci.edu/perl/WebSoc', data =
-                      {'Breadth':'ANY','CancelledCourse':'Exclude','ClassType':'ALL',
-                       'CourseCodes':'36680','Dept':'ALL','Division':'ALL','FontSize':'100',
+    
+    r = requests.post('https://www.reg.uci.edu/perl/WebSoc', data ={'Breadth':'ANY','CancelledCourse':'Exclude','ClassType':'ALL',
+                        'CourseCodes':'36680','Dept':'ALL','Division':'ALL','FontSize':'100',
                        'FullCourses':'ANY', 'Submit':'Display Text Results', 'YearTerm':'2016-14'})
     text = r.text
     l = text.split('\n')
     info = l[24]
     info = info.split()
     length = len(info)
-    courseCode = info[0]
-    maxEnrolled = info[length-5-1]
-    enrolled = info[length-4-1]
+    courseCode = int(info[0])
+    maxEnrolled = int(info[length-5-1])
+    enrolled = int(info[length-4-1])
     waitlist = info[length-3-1]
 
     print(courseCode)
     print(maxEnrolled)
     print(enrolled)
     print(waitlist)
+
+    ACCOUNT_SID = "AC8563bbddc734434f3f0598d90f7c23c6" 
+    AUTH_TOKEN = "ead2b8568678dda16eb0380f80cdf7fa" 
+ 
+    client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
+ 
+    if (enrolled<maxEnrolled):
+        sms = client.messages.create(to = "+14158302607",
+                               from_="+16692312227",
+                            body = "Alert: Course enrolled < maxium enrollment:\n"+
+                                     str(enrolled) + " : " + str(maxEnrolled)) 
+    print(sms.sid)
 if __name__ == "__main__":
     main()
