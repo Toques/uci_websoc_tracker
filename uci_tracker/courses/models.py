8e164a5d5e943b71dce947fd4408b7ba6e669c71
Toques <calvinwong8@yahoo.com>
2016-06-21 20:57:16 -0700

    Started courses app
    Created course model and some tests

--- uci_tracker/courses/models.py
+++ uci_tracker/courses/models.py
@@ -3,16 +3,16 @@ from django.db import models
 # Create your models here.
 class Course(models.Model):
     course_code = models.CharField(max_length= 5)
-    max_enrollment = models.PositiveSmallIntegerField
-    current_enrollment = models.PositiveSmallIntegerField
-    wait_list = models.PositiveSmallIntegerField
+    max_enrollment = models.PositiveSmallIntegerField()
+    current_enrollment = models.PositiveSmallIntegerField()
+    wait_list = models.PositiveSmallIntegerField()
     restrictions = models.CharField(max_length = 5)
-    status = models.CharField(max_length = 4)
-    websoc_url = models.URLField
+    status = models.CharField(max_length = 7)
+    websoc_url = models.URLField()
 
     def __str__(self):
         return self.course_code
 
     def course_is_open(self):
-        return (status == 'OPEN' and current_enrollment < max_enrollment)
+        return (self.status == "OPEN" and self.current_enrollment < self.max_enrollment)
 
--- uci_tracker/courses/tests.py
+++ uci_tracker/courses/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
-
+from .models import Course
 # Create your tests here.
+class CourseMethodTests(TestCase):
+
+    def test_course_open(self):
+        c = Course(course_code= "12345",
+                    max_enrollment=300,
+                    current_enrollment=200,
+                    wait_list=0,
+                    restrictions="",
+                    status="OPEN",
+                    websoc_url=None)
+
+        self.assertTrue(c.course_is_open())
+
+    def test_course_open_new_only(self):
+        c = Course(course_code= "12345",
+                   max_enrollment=300,
+                   current_enrollment=200,
+                   wait_list=0,
+                   restrictions="",
+                   status="NewOnly",
+                   websoc_url=None)
+        self.assertFalse(c.course_is_open())
+
+    def test_course_full(self):
+        c = Course(course_code="12345",
+                   max_enrollment=300,
+                   current_enrollment=300,
+                   wait_list=0,
+                   restrictions="",
+                   status="FULL",
+                   websoc_url= None)
+        self.assertFalse(c.course_is_open())
+
+
+    def test_course_full_not_maxed_enrolled(self):
+        c = Course(course_code="12345",
+                   max_enrollment=300,
+                   current_enrollment=200,
+                   wait_list=0,
+                   restrictions="",
+                   status="FULL",
+                   websoc_url=None)
+        self.assertFalse(c.course_is_open())
